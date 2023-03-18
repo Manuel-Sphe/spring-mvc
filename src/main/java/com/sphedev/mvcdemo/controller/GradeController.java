b48e4509787a9a272159f86585bf6879e5a53c48
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-03-18 19:34:37 +0200

    Lombok Added

--- src/main/java/com/sphedev/mvcdemo/controller/GradeController.java
+++ src/main/java/com/sphedev/mvcdemo/controller/GradeController.java
@@ -1,12 +1,13 @@
-package com.sphedev.mvcdemo;
+package com.sphedev.mvcdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GradeController {
     @GetMapping("/grades")
-    public String getGrades(){
+    public String getGrades(Model model){
         return "grade";
     }
 }
---
+++ src/main/java/com/sphedev/mvcdemo/repository/Grade.java
@@ -0,0 +1,14 @@
+package com.sphedev.mvcdemo.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Grade {
+    private String name;
+    private String subject;
+    private String score;
+}
