7890245269b0e0b837a9499fb0ca0dc4ae5af25e
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-03-19 13:14:01 +0200

    Using tailwindcss

--- src/main/java/com/sphedev/mvcdemo/controller/GradeController.java
+++ src/main/java/com/sphedev/mvcdemo/controller/GradeController.java
@@ -1,13 +1,32 @@
 package com.sphedev.mvcdemo.controller;
 
+import com.sphedev.mvcdemo.repository.Grade;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class GradeController {
+    List<Grade> studentGrades = Arrays.asList(
+            new Grade("Harry","Football","C-",new Grade().getId()),
+            new Grade("Kane","Football","A-",new Grade().getId()),
+            new Grade("Ten Hag","Football","B",new Grade().getId()),
+            new Grade("Kimmich","Football","D",new Grade().getId())
+    );
     @GetMapping("/grades")
     public String getGrades(Model model){
-        return "grade";
+        model.addAttribute("grades",studentGrades);
+        return "grades";
     }
+
+    @GetMapping
+    public String gradeFrom(){
+        return "form";
+    }
+
 }
--- src/main/java/com/sphedev/mvcdemo/repository/Grade.java
+++ src/main/java/com/sphedev/mvcdemo/repository/Grade.java
@@ -3,12 +3,18 @@ package com.sphedev.mvcdemo.repository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.UUID;
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Grade {
     private String name;
     private String subject;
     private String score;
+    private  String id;
+
+
+    public Grade() {
+        this.id = UUID.randomUUID().toString();
+    }
+
 }
